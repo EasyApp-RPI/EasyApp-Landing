bc712abcbe4f84f7e9366e03cf50aaad5e76b337
David Chong <djc@ri-87-rsdh-wl-271.dynamic.rpi.edu>
2023-10-10 17:43:33 -0400

    started working on a landing page, created a header and working on conditionals for the header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "./Logo/easyAppDark.png"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+function Center(){
+  return(
+    <>
+      <h1 className='text-center'>Welcome to EasyApp</h1>
+    </>
   );
 }
 
-export default App;
+function Header({isLoggedIn}){
+  if(isLoggedIn){
+    return(
+      <>
+      <div className='px-5 d-flex'>
+        <div className='navbar navbar-expand-lg navbar-light'>
+            <nav className='pl-3'>
+              <a href = "#">
+                <img src = {logo} alt = "EasyApp Logo" className='' style={{width: "70px", height: "auto"}}/>
+              </a>
+            </nav>
+        </div>
+        <div className='navbar'>
+            <nav>
+              Settings
+            </nav>
+            <nav>
+              <button>
+                Download
+              </button>
+            </nav>
+            <nav>
+              <button>
+                Log In
+              </button>
+            </nav>
+            <nav>
+              <button className='button-primary'>
+                Sign up
+              </button>
+            </nav>
+        </div>
+      </div>
+      </>
+    )
+  }else{
+    return(
+      <>
+      <div className='px-5 d-flex justify-content-between'>
+        <div className='navbar navbar-expand-lg navbar-light'>
+            <nav className='pl-3'>
+              <a href = "#">
+                <img src = {logo} alt = "EasyApp Logo" className='' style={{width: "70px", height: "auto"}}/>
+              </a>
+            </nav>
+        </div>
+        <div className='navbar'>
+            <nav>
+              <button>
+                Download
+              </button>
+            </nav>
+            <nav>
+              <button type='button' className='btn btn-primary'>
+                Log In
+              </button>
+            </nav>
+            <nav>
+              <button type = 'button' className='btn btn-secondary'>
+                Sign up
+              </button>
+            </nav>
+        </div>
+      </div>
+      </>
+    );
+  }
+}
+
+function LandingPage() {
+
+  return(
+    <>
+      <Header isLoggedIn={false}/>
+      <Center />
+    </>
+  );
+}
+export default LandingPage;
